8b4f4ebe6023bc2ce30f9f1f3add8ef85dfc3a96
Junaid <junaidmangalwad.26@gmail.com>
2017-09-06 00:14:35 +0530

    Assignment 10.4

---
+++ app/src/main/java/com/example/jmush/android_session10_assignment_104/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.jmush.android_session10_assignment_104;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements ActionBar.TabListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Three tab to display in actionbar
+        ab.addTab(ab.newTab().setText("Audio").setIcon(R.drawable.ic_launcher).setTabListener(this));
+        ab.addTab(ab.newTab().setText("Video").setIcon(R.drawable.ic_launcher).setTabListener(this));
+
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+        //Called when a tab is selected
+        int nTabSelected = tab.getPosition();
+        switch (nTabSelected) {
+            case 0:
+                setContentView(R.layout.actionbar_audio);
+                break;
+            case 1:
+                setContentView(R.layout.actionbar_video);
+                break;
+        }
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // Called when a tab unselected.
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+        // Called when a tab is selected again.
+    }
+
+}
